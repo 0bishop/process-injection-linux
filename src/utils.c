32b6ce3679ed91ba567ee5e075e6fcc18330f504
bishop <87420893+0bishop@users.noreply.github.com>
2023-05-28 17:05:37 +0200

    feat(scope): add root check and scope !

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,24 @@
 #include "../include/lib.h"
 
+extern bool get_scope(void)
+{
+    int fd = open("/proc/sys/kernel/yama/ptrace_scope", O_RDONLY);
+    char *buffer = NULL;
+
+    if (fd < 0)
+        return true;
+    buffer = malloc(2);
+    read(fd, buffer, 1);
+    close(fd);
+    buffer[1] = '\0';
+    if (!strcmp(buffer, "0")) {
+        free(buffer);
+        return false;
+    }
+    free(buffer);
+    return true;
+}
+
 extern int put(int fd, char *str)
 {
     return write(fd, str, strlen(str));
