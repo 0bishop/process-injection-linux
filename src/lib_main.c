12e5c8d67839d0c8b91b95b8d32a12f704ef730f
bishop <87420893+0bishop@users.noreply.github.com>
2023-05-28 17:08:36 +0200

    feat(moving root check) : move root check into another commands

--- src/lib_main.c
+++ src/lib_main.c
@@ -11,6 +11,10 @@ extern int inject_code(char *filename, char *function_name, char *filename_shell
 {
     info_t *inf = NULL;
 
+    if (geteuid() && get_scope()) {
+        put(2, "You must be root to run this program.\n");
+        return 1;
+    }
     return (!(inf = init_struct(filename, function_name, filename_shellcode)) ||
     !(inf->function_adress = get_function_adress(inf)) ||
     !injection(inf)) ? -1 : free_struct(inf);
